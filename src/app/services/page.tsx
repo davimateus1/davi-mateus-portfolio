0d95c1375f7effcb6495c086f6a670b8fda25d8b
Davi Mateus <davialmeida@atomstudio.com.br>
2024-07-19 22:16:56 -0300

    feat: create services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,75 @@
+"use client";
+
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { BsArrowDownRight } from "react-icons/bs";
+
+const services = [
+  {
+    index: 1,
+    title: "Desenvolvimento Web",
+    description:
+      "Desenvolvimento de sites, sistemas e aplicações web. Utilizando as melhores tecnologias do mercado, entregando soluções modernas e eficientes.",
+  },
+  {
+    index: 2,
+    title: "Desenvolvimento Mobile",
+    description:
+      "Desenvolvimento de aplicativos móveis para Android e iOS. Tenha um aplicativo exclusivo para a sua empresa ou negócio.",
+  },
+  {
+    index: 3,
+    title: "SEO",
+    description:
+      "Otimização de sites para mecanismos de busca. Aumente a visibilidade do seu site e alcance mais clientes.",
+  },
+  {
+    index: 4,
+    title: "Hospedagem",
+    description:
+      "Hospedagem de sites e sistemas web. Tenha um serviço de hospedagem seguro e confiável.",
+  },
+];
+
 const Services = () => {
-    return <div>services</div>;
-  };
-  
-  export default Services;
-  
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map(({ description, index, title }) => (
+            <div
+              key={index}
+              className="flex-1 flex flex-col justify-center gap-6 group"
+            >
+              <div className="w-full flex justify-between items-center">
+                <h1 className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                  {String(index).padStart(2, "0")}
+                </h1>
+                <Link
+                  href={`/teste`}
+                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                >
+                  <BsArrowDownRight className="text-primary text-3xl" />
+                </Link>
+              </div>
+              <h2
+                className="text-[30px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500"
+              >{title}</h2>
+              <p className="text-white/60">{description}</p>
+              <div className="border-b border-white/20 w-full" />
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
